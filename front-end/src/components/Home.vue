9c8ef83b3d21e948cb2f070cf25978193e2f2b45
JakuJ <janaszkiewicz.jakub@gmail.com>
2020-03-27 19:05:17 +0100

    Added the front-end

---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="title">Play 2048</h1>
+    <board-view ref="boardView"></board-view>
+    <button class="defaultButton" @click="botMode">Let bot play</button>
+  </div>
+</template>
+
+<script>
+import BoardView from "./BoardView.vue";
+export default {
+  components: { BoardView },
+  methods: {
+    botMode() {
+      this.$refs.boardView.deregisterCallbacks();
+      // TODO: Tutaj podpiąć bota
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
