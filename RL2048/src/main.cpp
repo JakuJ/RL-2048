6d96de335c915a6aa3f5a4d6aa3f7b3ccd295c99
JakuJ <janaszkiewicz.jakub@gmail.com>
2020-04-27 22:12:33 +0200

    Skrypt + powrót kresek

--- RL2048/src/main.cpp
+++ RL2048/src/main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <memory>
+#include <omp.h>
 #include "../headers/Ensemble.hpp"
 #include "../headers/TD.hpp"
 #include "../headers/ScoreWriter.hpp"
-
-#ifdef _OPENMP
-
-#include <omp.h>
-
-#endif
 
 int main(int argc, char *argv[]) {
     // Handle CLI arguments
@@ -22,23 +17,25 @@ int main(int argc, char *argv[]) {
     char *filename = argv[3];
 
     // Setup OpenMP
-#ifdef _OPENMP
-    std::cout << "Max threads: " << omp_get_max_threads() << std::endl;
-#endif
+    std::cout << "Available threads: " << omp_get_max_threads() << std::endl;
     // Initialize the model
     std::unique_ptr<Model> model = std::make_unique<Ensemble>(learning_rate);
     ScoreWriter scoreWriter(filename);
 
     // Train the model
+    int total = 0;
 
-#pragma omp parallel default(none) shared(epochs, model, scoreWriter, std::cout)
+#pragma omp parallel default(none) shared(epochs, model, scoreWriter, std::cout, total)
     {
-#ifdef _OPENMP
 #pragma omp single
         {
             std::cout << "Using " << omp_get_num_threads() << " threads" << std::endl;
         }
-#endif
+
+        const char *red("\033[0;31m");
+        const char *green("\033[0;32m");
+        const char *reset("\033[0m");
+
 #pragma omp for schedule(dynamic) nowait
         for (int epoch = 1; epoch <= epochs; epoch++) {
             auto[board, score] = playGame(model.get());
@@ -47,15 +44,28 @@ int main(int argc, char *argv[]) {
 
 #pragma omp critical
             {
+                switch (max) {
+                    case 2048:
+                        std::cout << green << "|" << reset;
+                        break;
+                    case 4096:
+                        std::cout << red << "X" << reset;
+                        break;
+                    case 8192:
+                        std::cout << red << "@" << reset;
+                        break;
+                    default:
+                        std::cout << ".";
+                        break;
+                }
                 scoreWriter.log(score, max);
             }
 
-            if (epoch % 100 == 0) {
-                std::cout << " " << epoch << " / " << epochs;
-#ifdef _OPENMP
-                std::cout << " | Thread " << omp_get_thread_num();
-#endif
-                std::cout << std::endl;
+#pragma omp atomic
+            total++;
+
+            if (total % 100 == 0) {
+                std::cout << " " << total << " / " << epochs << std::endl;
             }
         }
     }
