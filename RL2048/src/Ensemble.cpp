99394867650df8f2554c231c1f64b6c0fd81ddb2
JakuJ <janaszkiewicz.jakub@gmail.com>
2020-06-14 19:08:30 +0200

    Kolorki i nowy model na masterze

--- RL2048/src/Ensemble.cpp
+++ RL2048/src/Ensemble.cpp
@@ -5,73 +5,27 @@
 
 Ensemble::Ensemble(double lr) : learning_rate(lr) {
     constexpr int m = 15;
-    tuples.reserve(17);
 
-    for (int i = 0; i < Board::size; ++i) {
-        std::tuple<int, int> row[4] = {{0, i},
-                                       {1, i},
-                                       {2, i},
-                                       {3, i}};
+    std::tuple<int, int> shape[6] = {{1, 1},
+                                     {1, 0},
+                                     {0, 0},
+                                     {0, 1},
+                                     {0, 2},
+                                     {0, 3}};
 
-        std::tuple<int, int> column[4] = {{i, 0},
-                                          {i, 1},
-                                          {i, 2},
-                                          {i, 3}};
+    for (auto &&t: SymmetryExpander::expand<6>(m, shape))
+        tuples.push_back(std::move(t));
 
-        tuples.emplace_back(new NTuple<4>{m, row});
-        tuples.emplace_back(new NTuple<4>{m, column});
-    }
+    std::tuple<int, int> other_shape[6] = {{2, 1},
+                                           {2, 0},
+                                           {1, 0},
+                                           {1, 1},
+                                           {1, 2},
+                                           {1, 3}};
 
-    for (int i = 0; i < Board::size - 1; ++i) {
-        for (int j = 0; j < Board::size - 1; ++j) {
-            std::tuple<int, int> square[4] = {
-                    {i,     j},
-                    {i + 1, j},
-                    {i + 1, j + 1},
-                    {i,     j + 1},
-            };
+    for (auto &&t: SymmetryExpander::expand<6>(m, other_shape))
+        tuples.push_back(std::move(t));
 
-            tuples.emplace_back(new NTuple<4>{m, square});
-        }
-    }
-
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            std::tuple<int, int> rectangle[6]{{i,     j},
-                                              {i,     j + 1},
-                                              {i + 1, j},
-                                              {i + 1, j + 1},
-                                              {i + 2, j},
-                                              {i + 2, j + 1}};
-
-            tuples.emplace_back(new NTuple<6>{m, rectangle});
-        }
-    }
-
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            std::tuple<int, int> rectangle[6]{{i,     j},
-                                              {i + 1, j},
-                                              {i,     j + 1},
-                                              {i + 1, j + 1},
-                                              {i,     j + 2},
-                                              {i + 1, j + 2}};
-
-            tuples.emplace_back(new NTuple<6>{m, rectangle});
-        }
-    }
-
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            std::tuple<int, int> mini_col[2]{{i,     j},
-                                             {i + 1, j}};
-            std::tuple<int, int> mini_row[2]{{j, i},
-                                             {j, i + 1}};
-
-            tuples.emplace_back(new NTuple<2>{m, mini_col});
-            tuples.emplace_back(new NTuple<2>{m, mini_row});
-        }
-    }
 }
 
 double Ensemble::apply(const Board &board) const {
--- RL2048/src/main.cpp
+++ RL2048/src/main.cpp
@@ -53,16 +53,19 @@ int main(int argc, char *argv[]) {
                 scoreWriter.log(score2, max);
                 switch (max) {
                     case 2048:
-                        std::cout << GREEN "█" RESET;
+                        std::cout << "█";
                         break;
                     case 4096:
-                        std::cout << BLUE "█" RESET;
+                        std::cout << GREEN "█" RESET;
                         break;
                     case 8192:
-                        std::cout << RED "█" RESET;
+                        std::cout << BLUE "█" RESET;
                         break;
                     case 16384:
-                        std::cout << "\n" RED "** 16K **" RESET "\n";
+                        std::cout << RED "█" RESET;
+                        break;
+                    case 32768:
+                        std::cout << "\n" RED "*** 32K ***" RESET "\n";
                         break;
                     default:
                         std::cout << ' ';
@@ -75,11 +78,6 @@ int main(int argc, char *argv[]) {
 
             if (total % 100 == 0) {
                 std::cout << ' ' << total << " / " << epochs << '\n';
-
-                // Unsafe?
-                if (total % 50000 == 0) {
-                    model->save_model(save_prefix + std::to_string(total / 1000) + "k_");
-                }
             }
         }
     }
